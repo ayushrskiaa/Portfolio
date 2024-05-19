bdd21722d6ac2341da9b92c52c44804e3804af34
ayush <ayushkumar2263@iiitmanipur.ac.in>
2024-05-19 08:03:00 +0530

    changed

---
+++ main.js
@@ -0,0 +1,15 @@
+var typeData = new Typed(".role", {
+    strings: [
+      "Web Developer",
+      "UI/UX Designer",
+      "Web Designer",
+      "Competitive Coder",
+      
+    ],
+    loop: true,
+    typeSpeed: 100,
+    backSpeed: 60,
+    backDelay: 100,
+  });
+
+  
